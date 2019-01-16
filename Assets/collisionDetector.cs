07cf752f9f907742246841bdfc1c53540af7e87c
Taylor Sullivan <taylorgsullivan@yahoo.com>
2019-01-16 01:42:28 -0500

    Init

---
+++ Assets/collisionDetector.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionDetector : MonoBehaviour {
+    Color currentColor = Color.yellow;
+
+    public AudioClip mySound;
+    public float volume;
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+       // print("CollisionNNN BITCH");
+       // Shader cube = Shader.Find("cube");
+      //  gameObject.GetComponent<Renderer>().material.color = Color.green;
+        if(collision.gameObject.name != "Plane") {
+            toggleColor();
+            if (audio)
+            {
+                audio.PlayOneShot(mySound, volume);
+            }
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        // print("CollisionNNN BITCH");
+        // Shader cube = Shader.Find("cube");
+        //  gameObject.GetComponent<Renderer>().material.color = Color.green;
+        if (collision.gameObject.name != "Plane")
+        {
+            toggleColor();
+        }
+    }
+
+    private void toggleColor()
+    {
+        if(currentColor == Color.yellow){
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+            currentColor = Color.green;
+        }else {
+            gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+            currentColor = Color.yellow;
+        }
+    }
+
+
+}
---
+++ Assets/cubeMove.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Threading;
+
+
+public class cubeMove : MonoBehaviour
+{
+
+    private static Thread clientRecieveThread;
+    static Vector3 position;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartClient();
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    static void StartClient() 
+    {
+        //try{
+            clientRecieveThread =  new Thread(new ThreadStart(GetMessages));
+            clientRecieveThread.IsBackground = true;
+            clientRecieveThread.Start();
+
+        //}
+        //catch (Exception e)
+        //{
+        //    print(e);
+        //}
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetMessages();
+        //transform.Translate(1f * Time.deltaTime , 0f , 0f);
+        //float m = 5;
+        //float x = Input.GetAxis("Horizontal");
+        //float y = Input.GetAxis("Vertical");
+        //transform.Translate(x * Time.deltaTime * m, 0f * m, y * Time.deltaTime * m);
+        print(position);
+        transform.position = position;
+    }
+
+    static void GetMessages ()
+    {
+        TcpClient client;
+        NetworkStream stream;
+
+        try
+        {
+            client = new TcpClient("127.0.0.1", 8000);
+
+            stream = client.GetStream();
+
+            while (true)
+            {
+                // if (stream == null) continue;
+
+                byte[] data = new byte[256];
+                int bytes = stream.Read(data, 0, data.Length);
+                const float scale = 50;
+                int x = (int)(sbyte)data[0];
+                int y = (int)(sbyte)data[1];
+                int z = (int)(sbyte)data[2];
+                print(x + " " +  y + " " + z);
+                position = new Vector3(x/scale, y /scale,z /scale);
+                print(position);
+
+            }
+        }
+        catch (SocketException e)
+        {
+            print(e);
+        }
+
+ 
+    }
+
+    //private void OnDestroy()
+    //{
+    //    if (stream != null)
+    //    {
+    //        stream.Close();
+    //    }
+    //    if (client != null)
+    //    {
+    //        client.Close();
+    //    }
+    //}
+
+}
