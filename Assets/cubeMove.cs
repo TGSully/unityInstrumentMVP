ce36caf201ee86d0bc7e5b816076cd48c7c9eb7a
Taylor Sullivan <taylorgsullivan@yahoo.com>
2019-03-05 18:14:51 -0500

    new layout

--- Assets/cubeMove.cs
+++ Assets/cubeMove.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Sockets;
 using System.Threading;
+using System;
 
 
 public class cubeMove : MonoBehaviour
@@ -42,8 +43,25 @@ public class cubeMove : MonoBehaviour
         //float x = Input.GetAxis("Horizontal");
         //float y = Input.GetAxis("Vertical");
         //transform.Translate(x * Time.deltaTime * m, 0f * m, y * Time.deltaTime * m);
-        print(position);
-        transform.position = position;
+
+        // Vector3 lastPosition = transform.position;
+        // float fr = Math.Min(Math.Abs(lastPosition.z - position.z) / 4.0f + 0.15f, 1);
+        // float r = lastPosition.z * (1 - fr) + position.z * fr;
+        // float fx = Math.Min(Math.Abs(lastPosition.x - position.x) / 7.0f + 0.15f, 1);
+        // float x = lastPosition.x * (1 - fx) + position.x * fx;
+        // float fy = Math.Min(Math.Abs(lastPosition.y - position.y) / 7.0f + 0.15f, 1);
+        // float y = lastPosition.y * (1 - fy) + position.y * fy;
+        //float m = 0.5;
+        Vector3 translation = position - transform.position;
+        //print(translation);
+        //transform.Translate(translation.x * Time.deltaTime * m, translation.y * Time.deltaTime * m, translation.z * Time.deltaTime * m);
+        transform.Translate(translation);
+        //Vector3 p = new Vector3(x, y, r);
+        //print(position);
+
+        //transform.position = position;
+
+        //transform.position = Vector3.Lerp(lastPosition, position, 0.9f);
     }
 
     static void GetMessages ()
@@ -63,13 +81,13 @@ public class cubeMove : MonoBehaviour
 
                 byte[] data = new byte[256];
                 int bytes = stream.Read(data, 0, data.Length);
-                const float scale = 50;
+                const float scale = 30;
                 int x = (int)(sbyte)data[0];
                 int y = (int)(sbyte)data[1];
                 int z = (int)(sbyte)data[2];
-                print(x + " " +  y + " " + z);
-                position = new Vector3(x/scale, y /scale,z /scale);
-                print(position);
+                //print(x + " " +  y + " " + z);
+                position = new Vector3(x/scale, y /scale, -1f * z /scale);
+                //print(position);
 
             }
         }
